83ec41002caddbf88153bcdfc79dd08636f359e6
R3enox <129979133+R3enox@users.noreply.github.com>
2023-12-09 10:43:58 +0200

    do all tasks

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -3,6 +3,9 @@ import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
+type SelectedMenu = { id?: MenuIds }
+type MenuSelected = { selectedMenu: SelectedMenu }
+type MenuAction = { onSelectedMenu: (obj: SelectedMenu) => void }
 
 // Додати тип Menu Selected
 
@@ -17,7 +20,8 @@ const MenuActionContext = createContext<MenuAction>({
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode
+  // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +52,8 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]
+  // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
